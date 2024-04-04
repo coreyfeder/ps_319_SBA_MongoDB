8b2e9806a46fb18c0467caf4734fd23291ad66de
Corey Feder <corey.feder@gmail.com>
2024-04-03 23:59:35 -0400

    updates

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// import express from "express"
 const express = require("express");
 // const bodyParser = require("body-parser")
-const fs = require("fs");
-const util = require("node:util");
-const { exit } = require("process");
-const mongoose = require("mongoose");
+// const util = require("node:util");
+// const { exit } = require("process");
+// const mongoose = require("mongoose");
+const customers = require('./routes/customers.js');
 
 const app = express();
 const router = express.Router();
@@ -20,11 +19,6 @@ const port = 5555;
 const url = `${protocol}://${host}:${port}`;
 
 
-const dataFilename = "APIzza.json";
-const serverPath = ".";
-const dataFile = `${serverPath}/${dataFilename}`;
-
-
 /*
     Removed data read from flat file.
     TODO:
@@ -33,118 +27,6 @@ const dataFile = `${serverPath}/${dataFilename}`;
 
 
 // MIDDLEWARE
-
-function saveData() {
-    // TODO
-    return
-}
-
-
-function sanitizeString(text) {
-    // Sanitization function from mwag (https://stackoverflow.com/users/3160967/mwag) at StackOverflow, with edits
-    // https://stackoverflow.com/questions/24229262/match-non-printable-non-ascii-characters-and-remove-from-text/71459309#71459309
-    // strip control chars, trim, normalize whitespace
-    text = String(text).trim();
-    text = text.replace(/\p{C}/gu, "");
-    // overkill: normalize whitespace and strip newlines
-    text = text.replace(/\s/gu, " ");
-    text = text.replace(/[\n\r]+/g, " ");
-    text = text.replace(/[\p{Zl}]+/gu, " ");
-    text = text.replace(/[\p{Zp}]+/gu, " ");
-    text = text.replace(/[\p{Zs}]+/gu, " ");
-    text = text.replace(/\s/gu, " ");
-
-    return text;
-}
-
-function sanitizeObject(uncleanObject, validProperties) {
-    let cleanObject = {};
-    for (let property of validProperties) {
-        switch (typeof uncleanObject[property]) {
-            case "number":
-            case "boolean":
-                cleanObject[property] = uncleanObject[property];
-                break;
-            case "string":
-                cleanObject[property] = sanitizeString(uncleanObject[property]);
-                break;
-            case "object":
-                cleanObject[property] = JSON.parse(
-                    JSON.stringify(sanitizeString(uncleanObject[property])),
-                );
-                break;
-            default:
-                cleanObject[property] = null;
-        }
-    }
-    return cleanObject;
-}
-
-function sanitizeCustomer(potentialCustomer) {
-    let result = {success: null, error: null}
-    let newCustomer = sanitizeObject(potentialCustomer, [
-        "name",
-        "phone",
-        "address",
-        "delivery_notes",
-    ]);
-    if (newCustomer == {}) {
-        result.error = "Invalid customer data"
-        return result
-    }
-    let missingFields = ""
-    if (!newCustomer.name) { missingFields += ", Name"; }
-    if (!newCustomer.phone) { missingFields += ", Phone"; }
-    if (!newCustomer.address) { missingFields += ", Address"; }
-    missingFields = missingFields.slice(2)
-    if (missingFields) {
-        result.error = `Missing required fields for new customer: ${missingFields}`
-        return result
-    } else {
-        result.success = newCustomer
-        return result;
-    }
-}
-
-function sanitizeOrder(potentialOrder) {
-    console.log("potentialOrder")
-    console.log(potentialOrder)
-    let result = {success: null, error: null}
-    if (potentialOrder == {}) {
-        result.error = "Invalid order data"
-    } else if (!potentialOrder.customer_id) {
-        result.error = "Missing required fields for new order"
-    } else {
-        // no other sanitization. the customer is always right!
-        result.success = potentialOrder;
-    }
-    return result
-}
-
-function addNewOrder(newOrder) {
-    // keeping order number unique across launches
-    console.log(`adding new order`)
-    console.log(`current newOrder: ${newOrder}`)
-    console.log(`current order_count: ${data.orders_next_id}`)
-    newOrder.order_id = data.orders_next_id++;  // assign first, then increment
-    data.orders.push(newOrder);
-    saveData();
-    console.log(`new newOrder: ${newOrder}`)
-    console.log(`new order_count: ${data.orders_next_id}`)
-    return newOrder;
-}
-
-function findCustomerById(customer_id) {
-    return data.customers.find((item) => item.customer_id == customer_id)
-}
-
-function findOrderById(order_id) {
-    return data.orders.find((item) => item.order_id == order_id)
-}
-
-function findOrderIndexById(order_id) {
-    return data.orders.findIndex((item) => item.order_id == order_id)
-}
 
 // ROUTER
 
@@ -166,6 +48,7 @@ router.use((req, res, next) => {
 
 // ROUTES
 
+/*
 app.route("/customers").get(getCustomerById)
 app.route("/customers").post(postNewCustomer)
 app.route("/customers/:customer_id").get(getCustomerList)
@@ -177,7 +60,7 @@ app.route("/orders").get(getOrdersList);
 app.route("/orders/:order_id").get(getOrderById)
 app.route("/orders/:order_id").delete(deleteOrderById)
 app.route("/orders").post(postNewOrder);
-
+ */
 
 // ERROR HANDLING / endware
 //   If a call made it this far, something was wrong with it.
--- routes/customers.js
+++ routes/customers.js
@@ -1,14 +1,15 @@
 // TODO: MONGOIZE
-import customer from "../models/customer.mjs";
+// import customer from "../models/customer.mjs";
+const customer = require("../models/customer.mjs");
 
-export function getCustomerList(req, res) {
+function getCustomerList(req, res) {
     let customerList = customer.find().projection(exclude { '_id' })
     const customerList = await customer.find();
     console.log(customerList);//
     res.json(customerList);
 }
 
-export function getCustomerById(req, res) {
+function getCustomerById(req, res) {
     let foundCustomer = customer.findById(req.params.customer_id);
     if (foundCustomer) {
         res.json(foundCustomer);
@@ -17,10 +18,16 @@ export function getCustomerById(req, res) {
     }
 }
 
-export function postNewCustomer(newCustomer) {
+function postNewCustomer(newCustomer) {
     let maxCurrentCustomerId = data.customers[data.customers.length - 1].customer_id;
     newCustomer.customer_id = maxCurrentCustomerId + 1;
     data.customers.push(newCustomer);
     saveData();
     return newCustomer;
 }
+
+module.exports = {
+    getCustomerList,
+    getCustomerById,
+    postNewCustomer,
+};
